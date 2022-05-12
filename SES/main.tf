4aaae11207d32b8488c056b7415a9e81ba94bdc2
Jud4 <judavillalta@gmail.com>
2022-05-12 21:39:33 +0000

    Email identities with regions

--- SES/main.tf
+++ SES/main.tf
@@ -1,7 +1,9 @@
 resource "aws_ses_email_identity" "origin_mail_address" {
   email = "${var.sender_mail}"
+  provider = aws.ses_aws
 }
 
 resource "aws_ses_email_identity" "receiving_mail_address" {
   email = "${var.Secondmail}"
+  provider = aws.ses_aws
 }
